76d15a09138cd43840e5736bbebf8d59745b33f6
Devmidorka <urvanovdmitriy@gmail.com>
2022-06-11 13:48:07 +0300

    Delete unused files

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Button from "./UI/Button";
 import {ButtonType} from "../types/Button";
 import CartLogo from "./CartLogo";
-
+import logo from '../assets/logo.png'
 const NavBar = () => {
     return (
         <div className="navbar">
             <div>
-                <img src="/images/logo.png" alt=""/>
+                <img src={logo} alt=""/>
                 <div className="logo-info">
                     <h1>REACT PIZZA</h1>
                     <p>самая вкусная пицца во вселенной</p>
